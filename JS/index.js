c7a448227a5182b8187b8f1d901649badb812a54
victor <victor202@gmail.com>
2022-11-01 12:14:35 +0100

    HTML with title and test working

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,13 @@
-import strictEquals from './strictEquals';
+import strictEquals from './strictEquals.js';
 
-strictEquals();
+console.table([
+    ['A', 'B', 'RESULT'],
+    [1, 1, strictEquals(1, 1)],
+    [NaN, NaN, strictEquals(NaN, NaN)],
+    [0, -0, strictEquals(0, -0)],
+    [-0, 0, strictEquals(-0, 0)],
+    [1, '1', strictEquals(1, '1')],
+    [true, false, strictEquals(true, false)],
+    [false, false, strictEquals(false, false)],
+    ['water', 'oil', strictEquals(false, true)],
+]);
--- JS/strictEquals.js
+++ JS/strictEquals.js
@@ -1,5 +1,17 @@
-const strictEquals = () => {
-    console.log('hola mundo');
+const strictEquals = (a, b) => {
+    if (Object.is(a, b)) {
+        if (Object.is(a, NaN)) {
+            return !Object.is(a, b);
+        }
+        return Object.is(a, b);
+    }
+    if (
+        (Object.is(a, 0) && Object.is(b, -0)) ||
+        (Object.is(a, -0) && Object.is(b, 0))
+    ) {
+        return !Object.is(a, b);
+    }
+    return Object.is(a, b);
 };
 
 export default strictEquals;
